0e84df8c15206480a868524844045eb4cb8b66bd
bubenshchykov <a.bubenshchykov@gmail.com>
2017-04-27 14:10:55 +0300

    small cleanup

--- js/index.js
+++ js/index.js
@@ -1,30 +1,64 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
-    $http.get("contucts.json").then(function table (response) {
-        var arr = response.data.members;
-        $scope.myData = arr;
-    });
-    $scope.orderby=function(x){
-       $scope.order = x;
+var app = angular.module('app', []);
+
+var persons = [
+    {
+      "name": "Oliver",
+      "surname": "Kahn",
+      "phone": "240-094-35"
+    },
+    {
+      "name": "Chicharito",
+      "surname":"Ernandes",
+      "phone": "860-187-49"
+    },
+    {
+      "name": "Steven",
+      "surname":"Gerrard",
+      "phone": "461-597-11"
+    },
+    {
+      "name": "Frank",
+      "surname":"Lampard",
+      "phone": "298-488-74"
+    },
+    {
+      "name": "Zlatan",
+      "surname":"Ibragimovich",
+      "phone": "974-797-45"
     }
+  ];
+
+app.controller('customersCtrl', function($scope, $http) {
+
+    $scope.persons = persons;
+
+    $scope.orderBy = function(x) {
+       $scope.order = x;
+    };
+
     $scope.addPerson = function () {
-        if ($scope.Firstname && $scope.Secondname && $scope.Phonenumber){
-            $scope.flag = false;
-            for (var i = 0; i < $scope.myData.length; i++){
-                if ($scope.myData[i].Name.toLowerCase()== $scope.Firstname.toLowerCase() && $scope.myData[i].Surname.toLowerCase() == $scope.Secondname.toLowerCase()){
-                    $scope.flag = true
-                }
-            }
-            if ($scope.flag==false){
-                $scope.myData[$scope.myData.length]={
-                    Name:$scope.Firstname,
-                    Surname : $scope.Secondname,
-                    Phone : $scope.Phonenumber
-                }
-                $scope.Firstname = '';
-                $scope.Secondname = '';
-                $scope.Phonenumber = '';
-            }
+        if (!$scope.firstName || !$scope.secondName || !$scope.phoneNumber) {
+            return;
         }
+
+        $scope.isSamePerson = $scope.persons.some(isSamePerson);
+        if ($scope.isSamePerson) {
+            return;
+        }
+        
+        $scope.persons[$scope.persons.length] = {
+            name:$scope.firstName,
+            surname : $scope.secondName,
+            phone : $scope.phoneNumber
+        };
+
+        $scope.firstName = '';
+        $scope.secondName = '';
+        $scope.phoneNumber = '';
+    };
+
+    function isSamePerson(person) {
+        return person.name.toLowerCase() === $scope.firstName.toLowerCase() &&
+            person.surname.toLowerCase() === $scope.secondName.toLowerCase();
     }
 });
